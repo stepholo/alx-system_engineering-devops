5d71fed3eea1c10bba39e8261a4ea0750277b0b3
stepholo <oloobrian89@gmail.com>
2023-09-25 05:25:04 -0700

    Using puppet to config the ssh client configuration

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,11 +5,11 @@ include stdlib
 file_line { 'Refuse to authenticate using a password':
 	ensure => present,
 	path   => 'etc/ssh/ssh_config',
-	line   => 'PasswordAuthentication no',
+	line   => '	PasswordAuthentication no',
 }
 
 file_line { 'Use private key':
 	ensure => present,
 	path   => '/etc/ssh/ssh_config',
-	line   => 'IdentityFile ~/.ssh/school'
+	line   => '	IdentityFile ~/.ssh/school'
 }
